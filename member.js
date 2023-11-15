6924a5d9682499a329c61dcba371afdb58cc8240
Ryrii <aittouares_ryad@hotmail.fr>
2023-11-15 09:55:09 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,24 @@
+function skillsMember() {
+    var skills = {
+        name: "Jhon",
+        age: 22,
+        skills: ["C++", "Java", "Python", "Javascript"],
+        address: {
+            city: "New York",
+            country: "USA",
+        },
+    };
+    // console.log(skills.name);
+    // console.log(skills.age);
+    // console.log(skills.skills);
+    // console.log(skills.address.city);
+    // console.log(skills.address.country);
+    // console.log(skills["name"]);
+    // console.log(skills["age"]);
+    // console.log(skills["skills"]);
+    // console.log(skills["address"]["city"]);
+    // console.log(skills["address"]["country"]);
+    // console.log(skills.name, skills.age, skills.skills, skills.address.city, skills.address.country);
+    // console.log(skills["name"], skills["age"], skills["skills"], skills["address"]["city"], skills["address"]["country"]);
+    // console.log(skills.name + " " + skills.age + " " + skills.skills + " " + skills.address.city + " " + skills.address.country);
+    // console.log(skills["name"] + " " + skills["age"] + " " + skills["skills"] + " " + skills["address"]["city"] + " " + skills["address"]["country"]);
